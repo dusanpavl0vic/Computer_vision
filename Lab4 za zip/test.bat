50860f4ab5a1a77915bcf31b9b8caa99d06fc46b
Dusan Pavlovic <dusan.pavlovic016@gmail.com>
2025-09-27 15:15:21 +0200

    novo1

--- Lab4 za zip/test.bat
+++
@@ -1,7 +0,0 @@
-python deep_learning_with_opencv.py ^
---image images/space_shuttle.jpg ^
---prototxt bvlc_googlenet.prototxt ^
---model bvlc_googlenet.caffemodel ^
---labels synset_words.txt
-
-PAUSE
